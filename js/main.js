3d15d0d1c83e4673f4a747e4ea0083267b9a29d6
Alexey472 <101046394+Alexey472@users.noreply.github.com>
2022-08-16 15:35:24 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+$(function () {
+  
+  $(".main__slider-block").slick({
+    infinite: true,
+    dots: true,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  })
+
+  $(".main__reviews-slider").slick({
+    infinite: true,
+    dots: false,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  })
+
+
+});
+
+$(function () {
+  
+  $('.burger').on('click', function (e) {
+    e.preventDefault()
+    $('.header__inner-nav').toggleClass('header__inner-nav--open')
+  })
+
+
+});
+
